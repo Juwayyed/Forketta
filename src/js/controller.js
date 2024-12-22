7f89183e0a08fd5f7ddf97b363743186a658f02d
Juwayyed <166195145+Juwayyed@users.noreply.github.com>
2024-12-22 21:11:54 +0100

    MVC 75%

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,37 +6,27 @@ import { loadRecipe } from './model';
 
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
-const controlRecipes = async function() {
+const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
 
     if(!id) return;
-    recipeView.renderSpinner();
+    RecipeView.renderSpinner();
     
     // 1) Loading Recipe
     await model.loadRecipe(id);
-    const { recipe } = model.state; 
     
     // 2) Rendering Recipe
-    recipeView.render(model.state.recipe);
-    const recipeView = new recipeView(model.state.recipe);
+    RecipeView.render(model.state.recipe);
+
   } 
   catch(err) {
     alert(err);
   }
 }
 
-showRecipe();
-
-['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+const browserEvents = ['hashchange', 'load'];
+browserEvents.forEach(ev => window.addEventListener(ev, controlRecipes));
